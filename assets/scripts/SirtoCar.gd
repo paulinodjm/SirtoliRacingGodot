2899d6683ef6ced3144b3765375b3c0ac74fd98c
Paulino <paulinodjm@hotmail.com>
2019-03-25 13:22:27 +0100

    Add lateral friction

--- assets/scripts/SirtoCar.gd
+++ assets/scripts/SirtoCar.gd
@@ -3,6 +3,13 @@ extends RigidBody2D
 export var accel_speed = 90.0
 export var rotation_speed = 45.0
 
+const DRAW_AXIS_SCALE = 100
+const DRAW_VELOCITY_COLOR = Color.orange
+const DRAW_FORWARD_COLOR = Color.blue
+const DRAW_STRAFE_COLOR = Color.red
+const DRAW_AXIS_WEIGHT = 1
+const DRAW_FORCE_WEIGHT = 3
+
 func _ready():
 	pass
 
@@ -11,7 +18,29 @@ func _physics_process(delta):
 	set_angular_velocity(torque)
 	
 	var accel = accel_speed * get_move_input()
-	apply_impulse(Vector2(0,0), get_transform().y * accel)
+	apply_central_impulse(get_transform().y * accel)
+	
+	var lateral_friction = -linear_velocity.project(transform.x)
+	apply_central_impulse(lateral_friction)
+	
+func _process(delta):
+	update() # @DEBUG: triggers the canvas _draw function
+
+#func _draw():
+#	# Draws the local vectors
+#
+#	# Draws the global vectors
+#	draw_set_transform(Vector2.ZERO, -rotation, Vector2.ONE)
+#
+#	var move_forward = linear_velocity.project(transform.y)
+#	var move_strafe = linear_velocity.project(transform.x)
+#
+#	draw_line(Vector2.ZERO, linear_velocity, DRAW_VELOCITY_COLOR, DRAW_FORCE_WEIGHT)
+#	draw_line(Vector2.ZERO, transform.y * DRAW_AXIS_SCALE, DRAW_FORWARD_COLOR, DRAW_AXIS_WEIGHT)
+#	draw_line(Vector2.ZERO, transform.x * DRAW_AXIS_SCALE, DRAW_STRAFE_COLOR, DRAW_AXIS_WEIGHT)
+#	draw_line(Vector2.ZERO, move_forward, DRAW_FORWARD_COLOR, DRAW_FORCE_WEIGHT)
+#	draw_line(Vector2.ZERO, move_strafe, DRAW_STRAFE_COLOR, DRAW_FORCE_WEIGHT)	
+#	pass
 
 func get_rotation_speed():
 	return deg2rad(rotation_speed)
